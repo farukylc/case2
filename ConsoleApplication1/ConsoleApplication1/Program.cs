6e18f6ed573bc463db23ff6207645fe4ce828497
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-11-17 12:03:01 +0300

    u

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Dizi Boyutunu Girin: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.Write("Kurt Türlerini Girin: ");
+            int[] kurtDizisi = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sonuc = MostRepatedID(kurtDizisi);
+            Console.WriteLine("En Çok Tespit Edilen En Küçük ID: " + sonuc);
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz dizi boyutu. Lütfen bir tamsayı girin.");
+        }
+    }
+
+
+    static int MostRepatedID(int[] kurtDizisi)
+    {
+        Dictionary<int, int> tespitSayilari = new Dictionary<int, int>();
+
+        foreach (int kurtID in kurtDizisi)
+        {
+            if (tespitSayilari.ContainsKey(kurtID))
+                tespitSayilari[kurtID]++;
+            else
+                tespitSayilari[kurtID] = 1;
+        }
+
+        int enCokTespitEdilenID = tespitSayilari
+            .OrderByDescending(x => x.Value)
+            .ThenBy(x => x.Key)
+            .First().Key;
+
+        return enCokTespitEdilenID;
+    }
+}
